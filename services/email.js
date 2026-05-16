b21ff394c6867c8291e893b1a6adc3355a3f1651
Phpedrozo001-commits <Phpedrozo001@icloud.com>
2026-05-15 23:32:49 -0300

    Update email.js

--- services/email.js
+++ services/email.js
@@ -26,7 +26,7 @@ export async function sendEmail({ to, subject, html, text }) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        from: 'NeuralOps <onboarding@resend.dev>',
+        from: FROM_EMAIL,
         to: Array.isArray(to) ? to : [to],
         subject,
         html,
